eb468a4c5cab0b8d21a21d402e9d631fa84acf52
Julien Tissier <julient@embross.com>
2022-07-19 09:46:41 +0200

    Embed apollographql studio in dev mode enable introspection in dev mode

--- index.ts
+++ index.ts
@@ -3,14 +3,20 @@ import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server";
 import path from "path";
 import { PrismaClient } from "@prisma/client";
-
 import { resolvers } from "./prisma/generated/type-graphql";
+import {
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault
+} from "apollo-server-core";
+
 
 interface Context {
   prisma: PrismaClient;
 }
 
 async function main() {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  
   const schema = await buildSchema({
     resolvers,
     emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
@@ -42,8 +48,12 @@ async function main() {
   const server = new ApolloServer({
     schema,
     context: (): Context => ({ prisma }),
-	// https://www.apollographql.com/blog/graphql/security/why-you-should-disable-graphql-introspection-in-production/#:~:text=You%20can%20turn%20off%20introspection,%2C%20resolvers%2C%20introspection%3A%20process.
-	introspection: process.env.NODE_ENV !== 'production'
+    // https://www.apollographql.com/blog/graphql/security/why-you-should-disable-graphql-introspection-in-production/#:~:text=You%20can%20turn%20off%20introspection,%2C%20resolvers%2C%20introspection%3A%20process.
+    introspection: process.env.NODE_ENV !== 'production',
+    plugins: [
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === "production" ? ApolloServerPluginLandingPageProductionDefault({ footer: false }): ApolloServerPluginLandingPageLocalDefault({ footer: false, embed: true }),
+    ],
   });
   
   server.listen(4000).then(({ url }) => {
